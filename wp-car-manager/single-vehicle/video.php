acfb7d1404ec5a382cfe8d61c6e9b6696f10974c
Matias Hurtado <matias@fintual.com>
2020-06-11 00:37:38 -0400

    fix(): style video

--- wp-car-manager/single-vehicle/video.php
+++ wp-car-manager/single-vehicle/video.php
@@ -15,17 +15,23 @@ if (!empty($video)) {
 
 
 <div class="wpcm-vehicle-video-container row my-5">
-  <div class="col-12 col-md-4 wpcm-vehicle-video-info py-4 pl-4">
-    <h1 class="title d-block d-md-none"><i class="fa fa-video-camera"></i> Video en 360°</h1>
+  <div class="col-12 col-md-4 wpcm-vehicle-video-info pt-3 py-md-4 px-2 pl-md-4">
+    <h1 class="title d-block d-md-none">¡Revisa el auto con nuestro video 360!</h1>
     <h1 class="title display-2 d-none d-md-block"><i class="fa fa-video-camera"></i> 360°</h1>
-    <h1 class="subtitle mt-4 d-none d-md-block">Revisa el auto con el video en 360 grados</h1>
-    <button class="mt-5 d-none d-md-block">Ver el video ></button>
+    <h1 class="subtitle mt-4 d-none d-md-block">¡Revisa el auto con nuestro video 360!</h1>
+    <button id="playVideoBtn" class="mt-5 d-none d-md-block">Ver el video ></button>
   </div>
-  <div class="col-12 col-md-8 px-5 py-4">
-    <div class="wpcm-vehicle-video-embeded"><iframe width="100%" height="400" src="<?php echo $video; ?>?modestbranding=1&autohide=1&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe></div>
+  <div class="col-12 col-md-8 px-2 px-md-5 pt-2 pb-4 py-md-4">
+    <div class="wpcm-vehicle-video-embeded"><iframe id="videoIframe" width="100%" src="<?php echo $video; ?>?modestbranding=1&autohide=1&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe></div>
   </div>
 </div>
 
 <?php
 }
 ?>
+
+<script>
+  jQuery("#playVideoBtn").click(function(){
+    jQuery("#videoIframe")[0].src += "&autoplay=1";
+  });
+</script>
